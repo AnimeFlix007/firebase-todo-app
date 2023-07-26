d1d049963aea10cf27c3c5e90655edd66857ed40
AnimeFlix007 <devjitbose111@gmail.com>
2023-07-27 00:57:27 +0530

    Error Page

--- app/(todo-app)/error.tsx
+++ app/(todo-app)/error.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 type ErrorProps = {
   error: Error;
@@ -9,8 +8,25 @@ type ErrorProps = {
 };
 
 const Error = ({ error, reset }: ErrorProps) => {
-  const router = useRouter();
-  return error.message;
+  return (
+    <div className=" w-screen  h-[80vh] flex flex-col justify-center  items-center">
+      <Image
+        loading="lazy"
+        src="/images/error/ErrorPage.avif"
+        alt="ErrorImg"
+        width={350}
+        height={350}
+        style={{ borderRadius: "15px" }}
+      />
+      <button
+        onClick={reset}
+        type="button"
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4"
+      >
+        Try Again
+      </button>
+    </div>
+  );
 };
 
 export default Error;
